b3b87dcad05cec353591d46c825083e528ccd33a
anchh <anchalsharma31@gmail.com>
2023-03-01 13:37:48 +0530

    feat(mysql): add more insertion, delete, update functions
    function for getting, updating and deleting by id and name

--- Go/controller/controller.go
+++ Go/controller/controller.go
@@ -6,6 +6,7 @@ import (
 
 	"employee.db/myapp/model"
 	"employee.db/myapp/operation"
+	"github.com/gorilla/mux"
 )
 
 // Select all employees
@@ -46,6 +47,98 @@ func InsertEmployee(w http.ResponseWriter, r *http.Request) {
 	} else {
 		// Connect to SQL
 		response = operation.InsertEmployee_SQL(employee)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	json.NewEncoder(w).Encode(response)
+
+}
+
+// Delete Employee
+func DeleteEmployee(w http.ResponseWriter, r *http.Request) {
+	q_field := r.URL.Query()
+	db_name := q_field.Get("db")
+	var response model.Response
+	params := mux.Vars(r)
+	id := params["id"]
+	if db_name == "mongo" {
+		// Connect to Mongo
+	} else {
+		// Connect to SQL
+		response = operation.DeleteEmployee_SQL(id)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	json.NewEncoder(w).Encode(response)
+
+}
+
+// Update Employee
+func UpdateEmployee(w http.ResponseWriter, r *http.Request) {
+	var response model.Response
+
+	var employee model.Employee
+	err := json.NewDecoder(r.Body).Decode(&employee)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	params := mux.Vars(r)
+	id := params["id"]
+	employee.Id = id
+	q_field := r.URL.Query()
+	db_name := q_field.Get("db")
+	if db_name == "mongo" {
+		// Connect to Mongo
+	} else {
+		// Connect to SQL
+		response = operation.UpdateEmployee_SQL(employee)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	json.NewEncoder(w).Encode(response)
+
+}
+
+// Select employee by id
+func GetEmployee(w http.ResponseWriter, r *http.Request) {
+	var response model.Response
+
+	q_field := r.URL.Query()
+	db_name := q_field.Get("db")
+
+	params := mux.Vars(r)
+	id := params["id"]
+	if db_name == "mongo" {
+		// Connect to Mongo
+	} else {
+		// Connect to SQL
+		response = operation.GetEmployee_SQL(id)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	json.NewEncoder(w).Encode(response)
+
+}
+
+// Delete employee by name
+func DeleteEmployeeByName(w http.ResponseWriter, r *http.Request) {
+	var response model.Response
+
+	q_field := r.URL.Query()
+	db_name := q_field.Get("db")
+
+	params := mux.Vars(r)
+	name := params["name"]
+	if db_name == "mongo" {
+		// Connect to Mongo
+	} else {
+		// Connect to SQL
+		response = operation.DeleteEmployeeByName_SQL(name)
 	}
 
 	w.Header().Set("Content-Type", "application/json")
--- Go/operation/operationSQL.go
+++ Go/operation/operationSQL.go
@@ -70,3 +70,100 @@ func InsertEmployee_SQL(employee model.Employee) model.Response {
 	}
 	return response
 }
+
+// Delete Employee
+func DeleteEmployee_SQL(id string) model.Response {
+	var response model.Response
+
+	db := config.Connect_SQL()
+	defer db.Close()
+
+	delete_query := "DELETE FROM employee WHERE id = ?"
+	_, err := db.ExecContext(context.Background(), delete_query, id)
+
+	if err != nil {
+		log.Print(err.Error())
+		response.Status = 500
+		response.Message = "Failed to Delete"
+		response.Data = nil
+	} else {
+		response.Status = 200
+		response.Message = "Successfully Deleted"
+	}
+	return response
+}
+
+// Update Employee
+func UpdateEmployee_SQL(employee model.Employee) model.Response {
+	var response model.Response
+
+	db := config.Connect_SQL()
+	defer db.Close()
+
+	id := employee.Id
+	name := employee.Name
+	email := employee.Email
+	phone := employee.Phone
+	address := employee.Address
+	salary := employee.Salary
+
+	update_query := "UPDATE employee SET name = ?, email = ?, phone = ?, address = ?, salary = ? WHERE id = ?"
+	_, err := db.ExecContext(context.Background(), update_query, name, email, phone, address, salary, id)
+
+	if err != nil {
+		log.Print(err.Error())
+		response.Status = 500
+		response.Message = "Failed to Update"
+		response.Data = nil
+	} else {
+		response.Status = 200
+		response.Message = "Successfully Updated"
+	}
+	return response
+}
+
+// Get Employee by ID
+func GetEmployee_SQL(id string) model.Response {
+	var employee model.Employee
+	var response model.Response
+
+	db := config.Connect_SQL()
+	defer db.Close()
+
+	get_query := "SELECT * FROM employee WHERE id = ?"
+	err := db.QueryRowContext(context.Background(), get_query, id).Scan(&employee.Id, &employee.Name, &employee.Email, &employee.Phone, &employee.Address, &employee.Salary)
+
+	if err != nil {
+		log.Print(err.Error())
+		response.Status = 404
+		response.Message = "Not Found"
+		response.Data = nil
+	} else {
+		response.Status = 200
+		response.Message = "Success"
+		response.Data = append(response.Data, employee)
+	}
+	return response
+}
+
+// Delete Employee by name
+func DeleteEmployeeByName_SQL(name string) model.Response {
+	var response model.Response
+
+	db := config.Connect_SQL()
+	defer db.Close()
+
+	delete_query := "DELETE FROM employee WHERE name = ?"
+	_, err := db.ExecContext(context.Background(), delete_query, name)
+
+	if err != nil {
+		log.Print(err.Error())
+		response.Status = 500
+		response.Message = "Failed to Delete"
+		response.Data = nil
+	} else {
+		response.Status = 200
+		response.Message = "Successfully Deleted"
+	}
+	return response
+}
